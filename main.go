e3de87e100bf558ce554e81e6711669b297730fa
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2022-07-10 09:20:56 -0300

    finalizando implementacao

---
+++ main.go
@@ -0,0 +1,26 @@
+package api_postgresql
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/WilkerAlves/api-postgresql/configs"
+	"github.com/WilkerAlves/api-postgresql/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	err := configs.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Post("/", handlers.Create)
+	r.Put("/{id}", handlers.Update)
+	r.Delete("/{id}", handlers.Delete)
+	r.Get("/{id}", handlers.Get)
+	r.Get("/", handlers.List)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
+}
